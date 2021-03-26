68fe846820d519f6fb15244a822d39b787f488ed
MedKamal <moahemdkamalmourchid@gmail.com>
2021-03-26 20:06:47 +0100

    update mapper

--- src/app/Controllers/HomeController.php
+++ src/app/Controllers/HomeController.php
@@ -30,8 +30,18 @@ namespace Showcase\Controllers{
              *             2) all the <p> ======> Section->text
              * This figure in code goes like this : 
              *             Mapper::html($html)->queries(
-             *                         1) ['query' => 'h1', 'property' => 'title', 'object' => $section, 'return' => false],
-             *                         2) ['query' => 'p', 'property' => 'text', 'concatenate' => true, 'object' => $section])->first();
+             *                                          'query' => 'main_element_query',
+             *                                          'classes' => [
+             *                                                        [
+             *                                                          'name' => '\Showcase\Models\Section', // object class
+             *                                                          'options' => [
+             *                                                                  'queries' => [
+             *                                                1) get first h4 element ['query' => function($html) { return [$html->find('h1', 0)];}, 'property' => 'title'],
+             *                                                2) get first p element ['query' => function($html) { return [$html->find('p', 0)];}, 'property' => 'text']
+             *                                                                    ]
+             *                                                                   ]
+             *                                                           ]
+             *                                                          ])->map();
              * now the mapper will return an array with one object section with a valid title and a text
              */
 
@@ -59,8 +69,27 @@ namespace Showcase\Controllers{
                                         'queries' => [
                                             ['query' => '.MCDropDownBody.dropDownBody', 'sub' => 
                                                 [
-                                                    ['query' => 'h4', 'property' => 'title'],
-                                                    ['query' => 'p', 'property' => 'text']
+                                                    ['query' => function($html){
+                                                            $paragraph = [];
+                                                            // Get the starting node
+                                                            $startPoint = $html->find('h4', 0);
+                                                            if($startPoint != null) {
+                                                                // While the current node has a sibling
+                                                                while ( $next = $startPoint->next_sibling() ) {
+                                                                    // And as long as it's different from the end node => div.second
+                                                                    if ( $next->tag == 'h4')
+                                                                        break;
+                                                                    else{
+                                                                        // Print the content
+                                                                        $paragraph[] = $next;
+                                                                        // And move to the next node
+                                                                        $startPoint = $next;
+                                                                    }
+                                                                }
+                                                            }
+                                                            return $paragraph;
+                                                    }, 'property' => 'text'],
+                                                    ['query' => function($html) { return [$html->find('h4', 0)];}, 'property' => 'title']
                                                 ]
                                             ]
                                         ],
@@ -86,16 +115,57 @@ namespace Showcase\Controllers{
              * 
              * functions : 
              *  queries : set conditions and objects and queries
-             *  first : execute the queries and return an array and get first result
-             *  get: execute tje queries and return an array with all the results
+             *  map: execute the queries and return an array with all the results
              * 
              * queries keys : 
-             *  object: specify the object to save the html text to
-             *  property: the property where to save the data
-             *  query: simple_html_dom query, for more check manual => https://simplehtmldom.sourceforge.io/manual.htm
-             *  return: if you don't the object to be included in the returned array, set this to false, it's true by default
-             *  concatenate: if you want to concatenate all the results in one property, set this to true, it's false by default
+             *  query: the main query of the mapper, simple_html_dom query, for more check manual => https://simplehtmldom.sourceforge.io/manual.htm
+             *      Example: 
+             *          <div id="main">
+             *              <div class="wrapper">
+             *                  <p>some text here</p>
+             *              </div>
+             *          </div>
+             *  Ower main query would be to select the main div and then set the objects queries to specify the properties: 'query' => 'div[id=main]
+             *  classes: this key regroup all the objects mappers, with their queries and other options
              * 
+             * classes keys:
+             *  name: the name of the object, full namespace. Example \Showcase\Models\Model
+             *  options: this key regroup the queries for the object
+             * 
+             * options keys:
+             *  reference_to: if you want to use a property value of an object in other object (like foreign key), you have to set this to true:
+             *      Example:
+             *          Section(title, text) | Chapter (title, text, section_title)
+             *              in Section you would do:
+             *                [
+             *                 'name' => '\Showcase\Models\Section',
+             *                  'options' => [
+             *                      'queries' => [
+             *                        ],
+             *                        'reference_to' => true
+             *                      ]
+             *                 ]
+             *  reference_from: this it the other key setter for the reference_to, this tell the mapper to set a property value of the object, to another object property
+             *      Example:
+             *          'reference_from' => [
+             *                    'name' => '\Showcase\Models\Section',
+             *                    'reference_property' => 'title',
+             *                    'property' => 'section_title'
+             *                   ],
+             *      name: the name of the object to reference to
+             *      reference_property: the other object property ot reference to
+             *      property: this object property to set it's value
+             * 
+             *  multiple: if you want one object, don't set this, or set it to false, but if you want multiple objects, set this true
+             *  queries: the queries to map the object properties:
+             *      Example:
+             *          ['query' => 'h4', 'property' => 'title']
+             *      You need to set the query string and property name
+             *      The query string can be function
+             *          Example:
+             *          ['query' => function($html) { return [$html->find('h1', 0)];}, 'property' => 'title']
+             *              => Get the first h1 in the main node of the main query
+             *  
              */
             return self::response()->view('App/welcome');
         }
--- src/app/Models/Mapper.php
+++ src/app/Models/Mapper.php
@@ -8,9 +8,9 @@ namespace  Showcase\Models{
     class Mapper
     {
 
-        protected $_html;
-        protected $_conditions;
-        protected $references;
+        protected $_html; // current html page as string
+        protected $_conditions; // current queries to map with
+        protected $references; // current reference_to objects
 
         /**
          * Init the model
@@ -18,143 +18,260 @@ namespace  Showcase\Models{
         public function __construct(){
         }
         
+        /**
+         * Init the html to map
+         * 
+         * @param string $html page
+         * 
+         * @return SimpleHtmlDom
+         */
         public static function html($html) {
+            // init new mapper object
             $mapper = new self;
+            // init new SimpleHtmlDom object
             $mapper->_html = str_get_html($html);
+            // return mapper
             return $mapper;
         }
 
+        /**
+         * Set the array of queries to use
+         * 
+         * @param array $queries
+         * 
+         * @return \Showcase\Models\Mapper
+         */
         public function queries($queries) {
             $this->_conditions = $queries;
             return $this;
         }
 
+        /**
+         * Map the queries and html
+         * 
+         * @return array of mapped objects
+         */
         public function map() {
-            $this->references = [];
-            $list_objs = [];
+            // save last object to reference to
+            $this->references = []; 
+            // list of objects to return
+            $list_objs = []; 
+            // execute the main query and get the list of the returned nodes
             $html_object = $this->_html->find($this->_conditions['query']);
+            // loop on the returned nodes
             foreach($html_object as $_html) {
+                // loop on the given classes
                 foreach($this->_conditions['classes'] as $class) {
+                    // get the class name
                     $_class = $class['name'];
+                    // get the given queries for this class
                     $queries = $class['options']['queries'];
-
+                    // execute the mapping the get the nodes
                     $queries_results = $this->executeMapping($queries, $_html);
+                    // Check if the user want one object, or multiple objects
                     $multiple = false;
                     if(key_exists('multiple', $class['options'])) {
                         $multiple = filter_var($class['options']['multiple'], FILTER_VALIDATE_BOOLEAN);
                     }
 
                     if($multiple) {
+                        // if user want multiple objects, call this functions
                         $objs = $this->createObjects($_class, $queries_results, $class['options']);
+                        // sort the returne objects
                         foreach($objs as $obj)
                             $list_objs[$_class][] = $obj;
                     }else{
+                        // if the user want one object call this function
                         $obj = $this->createObject($_class, $queries_results);
+                        // Check if the object need references
                         $obj = $this->checkReferences($obj, $class['options'], $_class);
+                        // Sort object on the list
                         $list_objs[$_class][] = $obj;
                     }
                 }
             }
+            // return the list of objects
             return $list_objs;
         }
 
+        /**
+         * Execute queries and return array of nodes
+         * 
+         * @param array $queries to execute
+         * @param SimpleHtmlDom object
+         * 
+         * @return array of nodes
+         */
         private function executeMapping($queries, $html) {
+            // List of nodes to return
             $queries_results = [];
+            // loop into the queries
             for($i=0; $i < count($queries); $i++) {
+                // get the current query
                 $query = $queries[$i];
+                // check if the query is an array, it's need to be one
                 if(is_array($query)) {
-
+                    // check if this query, is composed, with a sub queries
                     if(key_exists('sub', $query)) {
+                        // if this has sub queries, execute the main query and the nodes
                         $sub_html = $this->executeQuery($query['query'], $html);
+                        // index for sorting the results into groups
+                        $index = 0;
+                        // loop into the returned nodes from the main query
                         foreach($sub_html as $_html) {
+                            // map the queries with the current node
                             $sub_queries_results = $this->executeMapping($query['sub'], $_html);
+                            // loop into the returned nodes, and sort them to the returning array
                             foreach($sub_queries_results as $property => $results){
-                                $queries_results[$property][] = $results;
+                                foreach($results as $res) {
+                                    $queries_results[$index][$property][] = $res;
+                                }
                             }
+                            // move forward
+                            $index++;
                         }
                     } else {
-                        foreach($this->executeQuery($query['query'], $html) as $c) {
-                            $queries_results[$query['property']][] = $c;
+                        // if this query don't have chilren
+                        // get the results nodes from execution
+                        $results = $this->executeQuery($query['query'], $html);
+                        // loop the results and sort them
+                        foreach($results as $node) {
+                            $queries_results[$query['property']][] = $node;
                         }
                     }
                 }
             }
-
+            // return the nodes
             return $queries_results;
         }
 
+        /**
+         * To create one object, with concatenated results
+         * 
+         * @param string $class name to init the object
+         * @param array of SimpleHtmlDom nodes
+         * 
+         * @return object of the $class
+         */
         private function createObject($class, $blocks) {
+            // init the object
             $obj = new $class;
-            foreach($blocks as $property => $block) {
-                foreach($block as $b) {
-                    $obj->{$property} .= $b->plaintext;
-                }
+            // loop into the nodes
+            foreach($blocks as $property => $node) {
+                foreach($node as $n)
+                    $obj->{$property} .= $n->plaintext; // set object properties with the values
             }
+            // return the object
             return $obj;
         }
 
+        /**
+         * This function create an array objects
+         * using the group sorting
+         * in the executeMapping function
+         * 
+         * @param string $class name to init objects
+         * @param array of SimpleHtmlDom nodes
+         * @param array $options of the query to check references
+         * 
+         * @return array of objects of the $class
+         */
         private function createObjects($class, $blocks, $options) {
+            // array of objects to return
             $objects = [];
-            $properties = array_keys($blocks);
-
-            for($i=0; $i < count($blocks[$properties[0]]); $i+=count($properties)){
+            // get the properties names of the objects (like $object->text)
+            $properties = array_keys($blocks[0]);
+            // loop into the nodes
+            for($i=0; $i < count($blocks); $i++) {
+                // init an object
                 $obj = new $class;
+                // loop into the properties
                 for($j=0; $j < count($properties); $j++) {
-                    $subBlocks = count($blocks[$properties[$j]]);
-                    for($k=0; $k < $subBlocks; $k++) {
-                        $property = $properties[$j];
-                        $obj->{$property} .= $blocks[$property][$k][$i+$j]->plaintext;
+                    // get the name of current property
+                    $property = $properties[$j];
+                    // if the nodes is not null, continue
+                    if(!is_null($blocks[$i])) {
+                        // if the property name exist in the node, continue
+                        if(key_exists($property, $blocks[$i])) {
+                            // get the nodes of the current property
+                            $nodes = $blocks[$i][$property];
+                            // loop into the nodes
+                            for($k=0; $k<count($nodes); $k++){
+                                // if the nodes is not null, continue
+                                if(!is_null($nodes[$k]))
+                                    $obj->{$property} .= $nodes[$k]->plaintext; // set the value of the property to the object
+                            }
+                        }
                     }
                 }
+                // check reference for this object
                 $obj = $this->checkReferences($obj, $options, $class);
+                // add object to the array
                 $objects[] = $obj;
             }
+            // return objects
             return $objects;
         }
 
-        private function checkReferences($obj, $options, $_class) {
+        /**
+         * Check the objects refeneces, like foreign keys
+         * 
+         * @param object to use it in the reference
+         * @param array $options of the query
+         * @param string $class name
+         * 
+         * @return object same object in the param
+         */
+        private function checkReferences($obj, $options, $class) {
+            // check if the query has reference_to this object
             if(key_exists('reference_to', $options)) {
+                // if the reference_to is set to true
                 if (filter_var($options['reference_to'], FILTER_VALIDATE_BOOLEAN)) {
-                    $this->references = ['name' => $_class, 'object' => $obj];
+                    // add this object to the refeneces array of this mapper
+                    $this->references = ['name' => $class, 'object' => $obj];
                 }
             }
 
+            // if this query has reference_from in the options
             if(key_exists('reference_from', $options)) {
+                // get the details of the other object
                 $element = $options['reference_from'];
                 /*$reference = array_filter($this->references, function($arr) use ($element) {
                     return $arr['name'] == $element['name'];
                 });*/
-    
+                // if the object we need to reference to it exist in the reference array
                 if($this->references['name'] === $element['name']) {
+                    // set this object property value with the reference_to object value
                     $obj->{$element['property']} = $this->references['object']->{$element['reference_property']};
                 }
             }
-
+            // return the object
             return $obj;
         }
 
-        private function executeQuery($query, $_html) {
+        /**
+         * Execute the query
+         * 
+         * @param mixte it can be a simple string or a function
+         * @param object SimpleHtmlDom object
+         */
+        private function executeQuery($query, $html) {
+            // if the query is string
             if(is_string($query))
-                $results = $_html->find($query);
+                $results = $html->find($query); // execute the string
             else {
-                $results = $query($_html);
+                // if the query is a function, execute the function
+                $results = $query($html);
             }
-                            
+
+            // if the results are array
             if(is_array($results)) {
+                // return them
                return $results;
             }
 
-            return $results;
-        }
-
-        private function getObject($list, $class) {
-
-            foreach($list as $obj) {
-                if(is_a($obj, $class))
-                    return $obj;
-            }
-            
-            return null;
+            // if not, add the results to an array and return it
+            return [$results];
         }
 
     }
